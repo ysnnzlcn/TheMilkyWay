28e77cf1732fae81803e59cb154918b84b247609
Yasin <yasin_nazlican@hotmail.com>
2021-11-21 19:13:09 +0300

    Fix unit tests

--- Core/Tests/CoreTests/Extensions/StringExtensionTests.swift
+++ Core/Tests/CoreTests/Extensions/StringExtensionTests.swift
@@ -11,13 +11,13 @@ import XCTest
 class StringExtensionTests: XCTestCase {
 
     func test_whenStringFormatGiven_thenCheckIfThereIsValidDate() {
-        let stabileDateString = "2002-03-20T00:00:00Z"
-        XCTAssertNotNil(stabileDateString.toDate(format: "yyyy-MM-dd"))
-        XCTAssertNotNil(stabileDateString.toDate(format: "yyyy-MM-dd HH"))
-        XCTAssertNotNil(stabileDateString.toDate(format: "yyyy-MM-dd HH:MM"))
-        XCTAssertNotNil(stabileDateString.toDate(format: "yyyy-MM-dd HH:mm:SS"))
-        XCTAssertNil(stabileDateString.toDate(format: "yyyy"))
-        XCTAssertNil(stabileDateString.toDate(format: "yyyy-MM"))
-        XCTAssertNil(stabileDateString.toDate(format: "yyyy-MM-dd"))
+        XCTAssertNotNil("2002-03-20".toDate(format: "yyyy-MM-dd"))
+        XCTAssertNotNil("2002-03-20 02".toDate(format: "yyyy-MM-dd HH"))
+        XCTAssertNotNil("2002-03-20 02:00".toDate(format: "yyyy-MM-dd HH:mm"))
+        XCTAssertNotNil("2002-03-20 02:00:00".toDate(format: "yyyy-MM-dd HH:mm:ss"))
+        XCTAssertNotNil("20-03-2002".toDate(format: "dd-MM-yyyy"))
+        XCTAssertNil("2002-03-20 02:00".toDate(format: "yyyy-MM-dd"))
+        XCTAssertNil("2002-03-20 02".toDate(format: "yyyy-MM-dd"))
+        XCTAssertNil("2002-03".toDate(format: "yyyy-MM-dd"))
     }
 }
